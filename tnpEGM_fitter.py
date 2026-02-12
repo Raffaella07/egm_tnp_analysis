9de9f7e690b11242dabc2ebfee1d606c414b34dd
Raffaella07 <raffaella.tram@gmail.com>
2026-02-12 15:06:14 +0100

    update for --parquet run mode

--- tnpEGM_fitter.py
+++ tnpEGM_fitter.py
@@ -6,6 +6,14 @@ import sys
 import pickle
 import shutil
 from multiprocessing import Pool
+import numpy as np
+
+# Handle numpy compatibility for older versions
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", FutureWarning)
+    if not hasattr(np, "bool"):
+        np.bool = bool
 
 
 parser = argparse.ArgumentParser(description='tnp EGM fitter')
@@ -16,37 +24,64 @@ parser.add_argument('--sample'     , default='all'        , help = 'create histo
 parser.add_argument('--altSig'     , action='store_true'  , help = 'alternate signal model fit')
 parser.add_argument('--addGaus'    , action='store_true'  , help = 'add gaussian to alternate signal model failing probe')
 parser.add_argument('--altBkg'     , action='store_true'  , help = 'alternate background model fit')
-parser.add_argument('--altSigBkg'  , action='store_true'  , help = 'alternate signal and background model fit')
+parser.add_argument('--doPol'     , action='store_true'  , help = 'alternate background model fit- polynomial')
 parser.add_argument('--doFit'      , action='store_true'  , help = 'fit sample (sample should be defined in settings.py)')
 parser.add_argument('--mcSig'      , action='store_true'  , help = 'fit MC nom [to init fit parama]')
 parser.add_argument('--doPlot'     , action='store_true'  , help = 'plotting')
 parser.add_argument('--sumUp'      , action='store_true'  , help = 'sum up efficiencies')
 parser.add_argument('--iBin'       , dest = 'binNumber'   , type = int,  default=-1, help='bin number (to refit individual bin)')
 parser.add_argument('--flag'       , default = None       , help ='WP to test')
+parser.add_argument('--flag2'       , default = None       , help ='WP2 to test')
+parser.add_argument('--parquet'    , action='store_true'  , help = 'use Parquet input files (faster I/O, outputs ROOT histograms for compatibility)')
 parser.add_argument('settings'     , default = None       , help = 'setting file [mandatory]')
 
 
 args = parser.parse_args()
 
-print('===> settings %s <===' % args.settings)
-importSetting = 'import %s as tnpConf' % args.settings.replace('/','.').split('.py')[0]
-print(importSetting)
-exec(importSetting)
+print((args.flag))
+print((args.flag2))
+
+
+print(('===> settings %s <===' % args.settings))
+import importlib
+import importlib.util
+import os
+
+# Handle both relative and absolute paths for config file import
+settings_path = args.settings
+if not os.path.isabs(settings_path):
+    # If relative path, assume it's relative to current directory
+    settings_path = os.path.abspath(settings_path)
+
+print('Loading settings from: %s' % settings_path)
+
+# Use spec_from_file_location to import from absolute path
+spec = importlib.util.spec_from_file_location("tnpConf", settings_path)
+tnpConf = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(tnpConf)
 
 ### tnp library
 import libPython.binUtils  as tnpBiner
 import libPython.rootUtils as tnpRoot
 
+# Additional ROOT configuration for FFTW plugins
+try:
+    import ROOT as rt
+    rt.gErrorIgnoreLevel = rt.kError  # Suppress FFT plugin warnings
+    print("ROOT FFT configuration completed")
+except Exception as e:
+    print(f"Warning: Could not configure ROOT FFT: {e}")
 
-if args.flag is None:
+
+if args.flag is None and args.flag2 is None:
     print('[tnpEGM_fitter] flag is MANDATORY, this is the working point as defined in the settings.py')
     sys.exit(0)
     
-if not args.flag in tnpConf.flags.keys() :
-    print('[tnpEGM_fitter] flag %s not found in flags definitions' % args.flag)
+if not args.flag in list(tnpConf.flags.keys()) and not args.flag2 in list(tnpConf.flags.keys()) :
+    print(('[tnpEGM_fitter] flag %s not found in flags definitions' % args.flag))
     print('  --> define in settings first')
     print('  In settings I found flags: ')
-    print(tnpConf.flags.keys())
+    print((list(tnpConf.flags.keys())))
     sys.exit(1)
 
 outputDirectory = '%s/%s/' % (tnpConf.baseOutDir,args.flag)
@@ -62,8 +97,8 @@ if args.checkBins:
     tnpBins = tnpBiner.createBins(tnpConf.biningDef,tnpConf.cutBase)
     tnpBiner.tuneCuts( tnpBins, tnpConf.additionalCuts )
     for ib in range(len(tnpBins['bins'])):
-        print(tnpBins['bins'][ib]['name'])
-        print('  - cut: ',tnpBins['bins'][ib]['cut'])
+        print((tnpBins['bins'][ib]['name']))
+        print(('  - cut: ',tnpBins['bins'][ib]['cut']))
     sys.exit(0)
     
 if args.createBins:
@@ -73,9 +108,9 @@ if args.createBins:
     tnpBins = tnpBiner.createBins(tnpConf.biningDef,tnpConf.cutBase)
     tnpBiner.tuneCuts( tnpBins, tnpConf.additionalCuts )
     pickle.dump( tnpBins, open( '%s/bining.pkl'%(outputDirectory),'wb') )
-    print('created dir: %s ' % outputDirectory)
+    print(('created dir: %s ' % outputDirectory))
     print('bining created successfully... ')
-    print('Note than any additional call to createBins will overwrite directory %s' % outputDirectory)
+    print(('Note than any additional call to createBins will overwrite directory %s' % outputDirectory))
     sys.exit(0)
 
 tnpBins = pickle.load( open( '%s/bining.pkl'%(outputDirectory),'rb') )
@@ -84,67 +119,134 @@ tnpBins = pickle.load( open( '%s/bining.pkl'%(outputDirectory),'rb') )
 ####################################################################
 ##### Create Histograms
 ####################################################################
-for s in tnpConf.samplesDef.keys():
+for s in list(tnpConf.samplesDef.keys()):
     sample =  tnpConf.samplesDef[s]
     if sample is None: continue
     setattr( sample, 'tree'     ,'%s/fitter_tree' % tnpConf.tnpTreeDir )
     setattr( sample, 'histFile' , '%s/%s_%s.root' % ( outputDirectory , sample.name, args.flag ) )
 
-
 if args.createHists:
 
-    print(" ======== Creating Histograms ========")
-    import libPython.histUtils as tnpHist
+	def parallel_hists(sampleType):
+		sample = tnpConf.samplesDef[sampleType]
+		if sample is None:
+			return
+		if not (sampleType == args.sample or args.sample == 'all'):
+			return
 
-    def parallel_hists(sampleType):
-        sample =  tnpConf.samplesDef[sampleType]
-        if sample is None : return
-        if sampleType == args.sample or args.sample == 'all' :
-            print('creating histogram for sample ')
-            sample.dump()
-            var = { 'name' : 'pair_mass', 'nbins' : 80, 'min' : 50, 'max': 130 }
-            if sample.mcTruth:
-                var = { 'name' : 'pair_mass', 'nbins' : 80, 'min' : 50, 'max': 130 }
-            tnpHist.makePassFailHistograms( sample, tnpConf.flags[args.flag], tnpBins, var )
-    
-    #pool = Pool()
-    #pool.map(parallel_hists, tnpConf.samplesDef.keys())
-    for k in tnpConf.samplesDef.keys(): parallel_hists(k)
+		print('creating histogram for sample ')
+		sample.dump()
 
-    sys.exit(0)
+		var = { 'name' : 'pair_mass', 'nbins' : 80, 'min' : 50, 'max': 130 }
+		if sample.mcTruth:
+			var = { 'name' : 'pair_mass', 'nbins' : 80, 'min' : 50, 'max': 130 }
+
+		# Choose histogram backend based on --parquet flag and format detection
+		if args.parquet:
+			try:
+				import libPython.histUtils_hybrid as tnpHist
+				print("Using hybrid histogram utilities (Parquet input to  ROOT histograms)")
+			except ImportError:
+				print("Warning: hybrid histogram utilities not available, falling back to original")
+				import libPython.histUtils as tnpHist
+		else:
+			import libPython.histUtils as tnpHist
+		
+		# Use unified interface - hybrid backend handles format detection
+		tnpHist.makePassFailHistograms(
+			sample,
+			tnpConf.flags[args.flag],
+			tnpConf.flags[args.flag2],
+			tnpBins,
+			var
+		)
+
+	pool = Pool()
+	pool.map(parallel_hists, list(tnpConf.samplesDef.keys()))
+	pool.close()
+	pool.join()
+	sys.exit(0)
 
 
 ####################################################################
 ##### Actual Fitter
 ####################################################################
-sampleToFit = tnpConf.samplesDef['data']
-if sampleToFit is None:
-    print('[tnpEGM_fitter, prelim checks]: sample (data or MC) not available... check your settings')
-    sys.exit(1)
 
-sampleMC = tnpConf.samplesDef['mcNom']
+# In parquet mode, we skip sample checks since histograms are pre-computed
+# but we verify that the histogram files exist
+if not args.parquet:
+    sampleToFit = tnpConf.samplesDef['data']
+    if sampleToFit is None:
+        print('[tnpEGM_fitter, prelim checks]: sample (data or MC) not available... check your settings')
+        sys.exit(1)
 
-if sampleMC is None:
-    print('[tnpEGM_fitter, prelim checks]: MC sample not available... check your settings')
-    sys.exit(1)
-for s in tnpConf.samplesDef.keys():
-    sample =  tnpConf.samplesDef[s]
-    if sample is None: continue
-    setattr( sample, 'mcRef'     , sampleMC )
-    setattr( sample, 'nominalFit', '%s/%s_%s.nominalFit.root' % ( outputDirectory , sample.name, args.flag ) )
-    setattr( sample, 'altSigFit' , '%s/%s_%s.altSigFit.root'  % ( outputDirectory , sample.name, args.flag ) )
-    setattr( sample, 'altBkgFit' , '%s/%s_%s.altBkgFit.root'  % ( outputDirectory , sample.name, args.flag ) )
-    setattr( sample, 'altSigBkgFit' , '%s/%s_%s.altSigBkgFit.root'  % ( outputDirectory , sample.name, args.flag ) )
+    sampleMC = tnpConf.samplesDef['mcNom']
+
+    if sampleMC is None:
+        print('[tnpEGM_fitter, prelim checks]: MC sample not available... check your settings')
+        sys.exit(1)
+        
+    for s in list(tnpConf.samplesDef.keys()):
+        sample =  tnpConf.samplesDef[s]
+        if sample is None: continue
+        setattr( sample, 'mcRef'     , sampleMC )
+        setattr( sample, 'nominalFit', '%s/%s_%s.nominalFit.root' % ( outputDirectory , sample.name, args.flag ) )
+        setattr( sample, 'altSigFit' , '%s/%s_%s.altSigFit.root'  % ( outputDirectory , sample.name, args.flag ) )
+else:
+    # Parquet mode: Check that pre-computed histogram files exist
+    print('[tnpEGM_fitter]: Running in Parquet mode - checking for pre-computed histograms...')
+    
+    # Check data histogram file
+    data_hist_file = '%s/data_Run3_2025_C_%s.root' % (outputDirectory, args.flag)
+    if not os.path.exists(data_hist_file):
+        print('[tnpEGM_fitter, prelim checks]: Data histogram file not found: %s' % data_hist_file)
+        print('  --> Run histogram creation phase first')
+        sys.exit(1)
+    else:
+        print('  ✓ Data histograms found: %s' % data_hist_file)
+    
+    # Check MC histogram file  
+    mc_hist_file = '%s/mc_Run3_2025_C_%s.root' % (outputDirectory, args.flag)
+    if not os.path.exists(mc_hist_file):
+        print('[tnpEGM_fitter, prelim checks]: MC histogram file not found: %s' % mc_hist_file)
+        print('  --> Run histogram creation phase first')
+        sys.exit(1)
+    else:
+        print('  ✓ MC histograms found: %s' % mc_hist_file)
+    
+    print('[tnpEGM_fitter]: All histogram files ready for fitting!')
+
+# For parquet mode, create minimal sample objects pointing to histogram files
+if args.parquet:
+    from libPython.tnpClassUtils import tnpSample
+    
+    # Create data sample pointing to existing histogram file
+    # Use dummy paths since histograms are pre-computed
+    sampleToFit = tnpSample('data', 'dummy_data_path.root', lumi=1.0)
+    sampleToFit.histFile = '%s/data_Run3_2025_C_%s.root' % (outputDirectory, args.flag)
+    sampleToFit.nominalFit = '%s/data_%s.nominalFit.root' % (outputDirectory, args.flag)
+    sampleToFit.altSigFit = '%s/data_%s.altSigFit.root' % (outputDirectory, args.flag)
+    sampleToFit.altBkgFit = '%s/data_%s.altBkgFit.root' % (outputDirectory, args.flag)
+    
+    # Create MC sample
+    sampleMC = tnpSample('mcNom', 'dummy_mc_path.root', lumi=1.0, isMC=True)
+    sampleMC.histFile = '%s/mc_Run3_2025_C_%s.root' % (outputDirectory, args.flag)
+    sampleMC.nominalFit = '%s/mc_%s.nominalFit.root' % (outputDirectory, args.flag)
+    sampleMC.altSigFit = '%s/mc_%s.altSigFit.root' % (outputDirectory, args.flag)
+    sampleMC.altBkgFit = '%s/mc_%s.altBkgFit.root' % (outputDirectory, args.flag)
+    
+    # Set references
+    sampleToFit.mcRef = sampleMC
 
 
 
 ### change the sample to fit is mc fit
 if args.mcSig :
-    sampleToFit = tnpConf.samplesDef['mcNom']
+    sampleToFit = sampleMC#tnpConf.samplesDef['mcNom']
+    sampleToFit.mcRef = sampleMC#tnpConf.samplesDef['mcNom']
 
 if  args.doFit:
-    print(" ======== Fitting ========")
-    sampleToFit.dump()
+#    sampleToFit.dump()
     def parallel_fit(ib):
         if (args.binNumber >= 0 and ib == args.binNumber) or args.binNumber < 0:
             if args.altSig and not args.addGaus:
@@ -152,14 +254,13 @@ if  args.doFit:
             elif args.altSig and args.addGaus:
                 tnpRoot.histFitterAltSig(  sampleToFit, tnpBins['bins'][ib], tnpConf.tnpParAltSigFit_addGaus, 1)
             elif args.altBkg:
+                # Use standard alternative background fit (no polynomial distinction for now)
                 tnpRoot.histFitterAltBkg(  sampleToFit, tnpBins['bins'][ib], tnpConf.tnpParAltBkgFit )
-            elif args.altSigBkg:
-                tnpRoot.histFitterAltSigBkg(  sampleToFit, tnpBins['bins'][ib], tnpConf.tnpParAltSigBkgFit )
             else:
                 tnpRoot.histFitterNominal( sampleToFit, tnpBins['bins'][ib], tnpConf.tnpParNomFit )
     pool = Pool()
-    pool.map(parallel_fit, range(len(tnpBins['bins'])))
-
+    pool.map(parallel_fit, list(range(len(tnpBins['bins']))))
+    #parallel_fit(range(len(tnpBins['bins']))[0])
     args.doPlot = True
      
 ####################################################################
@@ -174,9 +275,6 @@ if  args.doPlot:
     if args.altBkg : 
         fileName = sampleToFit.altBkgFit
         fitType  = 'altBkgFit'
-    if args.altSigBkg : 
-        fileName = sampleToFit.altSigBkgFit
-        fitType  = 'altSigBkgFit'
         
     os.system('hadd -f %s %s' % (fileName, fileName.replace('.root', '-*.root')))
 
@@ -197,22 +295,47 @@ if  args.doPlot:
 ##### dumping egamma txt file 
 ####################################################################
 if args.sumUp:
-    sampleToFit.dump()
-    info = {
-        'data'        : sampleToFit.histFile,
-        'dataNominal' : sampleToFit.nominalFit,
-        'dataAltSig'  : sampleToFit.altSigFit ,
-        'dataAltBkg'  : sampleToFit.altBkgFit ,
-        'dataAltSigBkg'  : sampleToFit.altSigBkgFit ,
-        'mcNominal'   : sampleToFit.mcRef.histFile,
-        'mcAlt'       : None,
-        'tagSel'      : None
+    if args.parquet:
+        # Parquet mode: construct info dict for pre-computed histograms
+        from libPython.tnpClassUtils import tnpSample
+        
+        # Create minimal sampleToFit object for parquet mode
+        sampleToFit = tnpSample('data', 'dummy_data_path.root', lumi=1.0)
+        sampleToFit.histFile = '%s/data_Run3_2025_C_%s.root' % (outputDirectory, args.flag)
+        
+        # Check if alternative fit files exist before adding to info dict
+        altSigFile = '%s/data_%s.altSigFit.root' % (outputDirectory, args.flag)
+        altBkgFile = '%s/data_%s.altBkgFit.root' % (outputDirectory, args.flag)
+        
+        # For parquet mode, use fit files if they exist, otherwise fallback to histograms
+        info = {
+            'data'           : '%s/data_Run3_2025_C_%s.root' % (outputDirectory, args.flag),
+            'dataNominal'    : '%s/data_%s.nominalFit.root' % (outputDirectory, args.flag),
+            'dataAltSig'     : altSigFile if os.path.exists(altSigFile) else None,
+            'dataAltBkg'     : altBkgFile if os.path.exists(altBkgFile) else None,
+            'dataAltSigBkg'  : None,  # Not implemented for parquet mode yet
+            'mcNominal'      : '%s/mc_Run3_2025_C_%s.root' % (outputDirectory, args.flag),
+            'mcAlt'          : None,
+            'tagSel'         : None
         }
+    else:
+        # Standard mode: use sample objects
+        sampleToFit.dump()
+        info = {
+            'data'        : sampleToFit.histFile,
+            'dataNominal' : sampleToFit.nominalFit,
+            'dataAltSig'  : sampleToFit.altSigFit ,
+            'dataAltBkg'  : sampleToFit.altBkgFit ,
+            'dataAltSigBkg': None,  # Add missing key
+            'mcNominal'   : sampleToFit.mcRef.histFile,
+            'mcAlt'       : None,
+            'tagSel'      : None
+            }
 
-    if not tnpConf.samplesDef['mcAlt' ] is None:
-       info['mcAlt'    ] = tnpConf.samplesDef['mcAlt' ].histFile
-    # if not tnpConf.samplesDef['tagSel'] is None:
-    #     info['tagSel'   ] = tnpConf.samplesDef['tagSel'].histFile
+    #if not tnpConf.samplesDef['mcAlt' ] is None:
+    #    info['mcAlt'    ] = tnpConf.samplesDef['mcAlt' ].histFile
+    #if not tnpConf.samplesDef['tagSel'] is None:
+     #   info['tagSel'   ] = tnpConf.samplesDef['tagSel'].histFile
 
     effis = None
     effFileName ='%s/egammaEffi.txt' % outputDirectory 
@@ -240,13 +363,23 @@ if args.sumUp:
             effis['dataAltBkg' ][0],
             effis['dataAltSig' ][0],
             effis['mcAlt' ][0],
-            # effis['tagSel'][0],
-            effis['dataAltSigBkg' ][0],
+            effis['tagSel'][0],
             )
         print(astr)
         fOut.write( astr + '\n' )
     fOut.close()
 
-    print('Effis saved in file : ',  effFileName)
+    print(('Effis saved in file : ',  effFileName))
     import libPython.EGammaID_scaleFactors as egm_sf
     egm_sf.doEGM_SFs(effFileName,sampleToFit.lumi)
+
+# Print status summary
+print("\n=== TnP Analysis Complete ===")
+if args.parquet:
+    print("Used Parquet mode: fast input reading with ROOT histogram output")
+    print("Histogram creation: Parquet to ROOT (hybrid)")
+    print("Fitting/plotting: Standard ROOT workflow")
+else:
+    print("Used standard ROOT mode: traditional workflow")
+    print("All operations used ROOT files")
+print(" Results ready for downstream analysis")
