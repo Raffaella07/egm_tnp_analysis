484744ce6efb14c62428f87281221d63a816e09b
Raffaella07 <raffaella.tram@gmail.com>
2026-02-12 14:53:58 +0100

    move py scripts to py3 + small tweaks for parquet integration

--- libPython/setup.py
+++ libPython/setup.py
@@ -1,32 +1,36 @@
 from distutils.core import setup
 from distutils.extension import Extension
-import sysconfig
+import os
 import sys
 
-# CFLAGS="$(root-config --cflags)" LDFLAGS="$(root-config --libs)" python setup.py build_ext --inplace
-
-# Trick from https://gist.github.com/ctokheim/6c34dc1d672afca0676a
-# to allow compilation directly from cpp file
 if '--use-cython' in sys.argv:
-    USE_CYTHON = True
-    sys.argv.remove('--use-cython')
+	USE_CYTHON = True
+	sys.argv.remove('--use-cython')
 else:
-    USE_CYTHON = False
+	USE_CYTHON = False
 ext = '.pyx' if USE_CYTHON else '.cpp'
 
-sourcefiles  = ["histUtils" + ext]
+sourcefiles = ["histUtils" + ext]
 
-extensions=[Extension('histUtils',
-            sourcefiles,
-            language='c++',
-            extra_compile_args=sysconfig.get_config_var('CFLAGS').split(),
-            extra_link_args= sysconfig.get_config_var('LDFLAGS').split(),
-            )]
+# Prendi flags dall'ambiente (se presenti)
+cxxflags = os.environ.get("CXXFLAGS", "")
+cflags = os.environ.get("CFLAGS", "")
+ldflags = os.environ.get("LDFLAGS", "")
+
+extra_compile = (cxxflags + " " + cflags).split()
+extra_link = ldflags.split()
+
+extensions = [Extension(
+	"histUtils",
+	sourcefiles,
+	language="c++",
+	extra_compile_args=extra_compile,
+	extra_link_args=extra_link,
+)]
 
 if USE_CYTHON:
-    from Cython.Build import cythonize
-    extensions = cythonize(extensions)
+	from Cython.Build import cythonize
+	extensions = cythonize(extensions)
 
-setup(
-  ext_modules=extensions
-)
+setup(ext_modules=extensions)
+
